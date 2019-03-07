4b5660b0aa81fd646584051f6b9a2a8b52180397
Sofonov <47359943+Sofonov@users.noreply.github.com>
2019-03-07 12:24:27 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "cadex_license_2.cxx"
+#include <cadex/Base_ProgressScope.hxx>
+#include <cadex/LicenseManager_Activate.h>
+#include <cadex/ModelData_Body.hxx>
+#include <cadex/ModelData_BodyList.hxx>
+#include <cadex/ModelData_BRepRepresentation.hxx>
+#include <cadex/ModelData_Direction.hxx>
+#include <cadex/ModelData_Face.hxx>
+#include <cadex/ModelData_Model.hxx>
+#include <cadex/ModelData_Model.hxx>
+#include <cadex/ModelData_OffsetSurface.hxx>
+#include <cadex/ModelData_Part.hxx>
+#include <cadex/ModelData_RectangularTrimmedSurface.hxx>
+#include <cadex/ModelData_Shape.hxx>
+#include <cadex/ModelData_Surface.hxx>
+#include <cadex/STEP_Reader.hxx>
+#include <cadex/STEP_ReaderParameters.hxx>
+
+using namespace std;
+using namespace cadex;
+
+
+
+int main(int argc, char *argv[])
+{
+
+	
+	if (!CADExLicense_Activate(aKey)) {
+		cerr << "Failed to activate CAD Exchanger license." << endl;
+		return 1;
+	}
+	const char* aSource = argv[1];
+
+
+	STEP_Reader aReader;
+	if (!aReader.ReadFile(aSource)) {
+		cerr << "Failed to read the file " << aSource << endl;
+		return 1;
+	}
+
+	return 0;
+}
+
